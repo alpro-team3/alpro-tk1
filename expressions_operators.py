85d7bdc93e3b6add111a719892bb3d3744a65b66
𝖗𝖆𝖉𝖏𝖆𝖆𝖆 𝖝𝖕 <119745631+raesone1@users.noreply.github.com>
2025-01-02 17:51:37 +0700

    compare

---
+++ expressions_operators.py
@@ -0,0 +1,10 @@
+# Ekspresi Perbandingan
+nilai_ujian = 85
+nilai_minimal = 75
+
+lulus = nilai_ujian >= nilai_minimal  # True jika nilai_ujian >= nilai_minimal
+print("Apakah lulus?", lulus)  # True
+
+# Ekspresi kompleks
+nilai_tinggi = nilai_ujian > 90
+print("Apakah nilai sangat tinggi?", nilai_tinggi)  # False
